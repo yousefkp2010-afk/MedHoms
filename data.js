1a1a60c10484b602a30ec3668c2149d80f3cd9fd
yousefkp2010-afk <yousefkp2010@gmail.com>
2026-02-01 19:08:51 +0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+function sayHi(msg){
+    console.log(msg);
+}
+
+let yousef = {
+    name: "yousef",
+    eat(){
+        console.log("he is eating")
+    },
+    age: 18
+}
+
+const ppl = [
+    {
+        name: "ali",
+        age: 12
+    },
+    {
+        name: "loi",
+        age: 11
+    
+    },
+    {
+        name: "mama",
+        age: 16
+        
+    },
+
+]
+
